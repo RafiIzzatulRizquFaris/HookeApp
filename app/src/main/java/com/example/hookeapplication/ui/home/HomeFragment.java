bbe49ebe76d51614b9a4004bc19307d39c980561
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2019-09-29 05:57:15 +0700

    Update HomeFragment.java

--- app/src/main/java/com/example/hookeapplication/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/hookeapplication/ui/home/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -12,6 +13,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.bumptech.glide.Glide;
 import com.example.hookeapplication.R;
 
 public class HomeFragment extends Fragment {
@@ -24,6 +26,8 @@ public class HomeFragment extends Fragment {
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
+        ImageView icHooke = root.findViewById(R.id.ic_hooke);
+        Glide.with(this).load(R.drawable.hookegif).into(icHooke);
         homeViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
