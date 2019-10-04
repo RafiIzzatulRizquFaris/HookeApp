39b308eb964963884fcc0372e50b1aa3784579ca
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2019-10-04 20:07:06 +0700

    Intent to GroupActivity

---
+++ app/src/main/java/com/example/hookeapplication/GroupActivity.java
@@ -0,0 +1,14 @@
+package com.example.hookeapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group);
+    }
+}
--- app/src/main/java/com/example/hookeapplication/MainActivity.java
+++ app/src/main/java/com/example/hookeapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.hookeapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -37,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, GroupActivity.class);
+                startActivity(intent);
             }
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
