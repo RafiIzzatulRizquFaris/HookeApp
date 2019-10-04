80311be524d1709341e95600f93af55c0af19bb2
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2019-10-04 21:33:43 +0700

    Updating Stress Fragment

--- app/src/main/java/com/example/hookeapplication/ui/stress/StressFragment.java
+++ app/src/main/java/com/example/hookeapplication/ui/stress/StressFragment.java
@@ -12,11 +12,16 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.hookeapplication.R;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textview.MaterialTextView;
 
 public class StressFragment extends Fragment {
 
     private StressViewModel stressViewModel;
+    TextInputEditText textInputEditTextluas, textInputEditTextgaya;
+    MaterialTextView materialTextView;
+    MaterialButton materialButton;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,6 +29,22 @@ public class StressFragment extends Fragment {
                 ViewModelProviders.of(this).get(StressViewModel.class);
         View root = inflater.inflate(R.layout.fragment_stress, container, false);
         final MaterialTextView textView = root.findViewById(R.id.title_stress);
+        textInputEditTextgaya = root.findViewById(R.id.edt_gaya_stress);
+        textInputEditTextluas = root.findViewById(R.id.edt_luas_stress);
+        materialButton = root.findViewById(R.id.btn_calculate_stress);
+        materialTextView = root.findViewById(R.id.tv_hasil_stress);
+        materialButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int gaya = Integer.parseInt(textInputEditTextgaya.getText().toString());
+                int luas = Integer.parseInt(textInputEditTextluas.getText().toString());
+                Double stress = (double) gaya+ (double) luas;
+                String hasil = String.valueOf(stress);
+                materialTextView.setText(hasil);
+            }
+        });
+
+
         stressViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
