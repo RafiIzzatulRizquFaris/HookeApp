b41bddb7e986b11d6ee3b24e6d9c464a13ca512d
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2019-10-05 06:56:10 +0700

    Updating id for mobile_navigation and drawerNav

--- app/src/main/java/com/example/hookeapplication/MainActivity.java
+++ app/src/main/java/com/example/hookeapplication/MainActivity.java
@@ -51,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_stress, R.id.nav_strain,
-                R.id.nav_modulus, R.id.nav_share, R.id.nav_send)
+                R.id.nav_modulus, R.id.nav_series, R.id.nav_parallel)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
