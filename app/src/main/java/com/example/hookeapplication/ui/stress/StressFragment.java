5ad7daf31d19c32cd3699160500cef8480fd7330
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2019-10-04 21:07:34 +0700

    Completing Stress Fragment

--- app/src/main/java/com/example/hookeapplication/ui/stress/StressFragment.java
+++ app/src/main/java/com/example/hookeapplication/ui/stress/StressFragment.java
@@ -4,27 +4,27 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.hookeapplication.R;
+import com.google.android.material.textview.MaterialTextView;
 
-public class GalleryFragment extends Fragment {
+public class StressFragment extends Fragment {
 
-    private GalleryViewModel galleryViewModel;
+    private StressViewModel stressViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        galleryViewModel =
-                ViewModelProviders.of(this).get(GalleryViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        galleryViewModel.getText().observe(this, new Observer<String>() {
+        stressViewModel =
+                ViewModelProviders.of(this).get(StressViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_stress, container, false);
+        final MaterialTextView textView = root.findViewById(R.id.title_stress);
+        stressViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
--- app/src/main/java/com/example/hookeapplication/ui/stress/StressViewModel.java
+++ app/src/main/java/com/example/hookeapplication/ui/stress/StressViewModel.java
@@ -4,11 +4,11 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class GalleryViewModel extends ViewModel {
+public class StressViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public GalleryViewModel() {
+    public StressViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is gallery fragment");
     }
