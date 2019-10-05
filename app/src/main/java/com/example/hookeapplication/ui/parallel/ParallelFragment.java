3653d8f1c873b2624c900c794b3ffc59af024b97
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2019-10-05 07:21:40 +0700

    Update ParallelFragment.java

--- app/src/main/java/com/example/hookeapplication/ui/parallel/ParallelFragment.java
+++ app/src/main/java/com/example/hookeapplication/ui/parallel/ParallelFragment.java
@@ -1,5 +1,6 @@
 package com.example.hookeapplication.ui.parallel;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,17 +14,38 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.hookeapplication.R;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textview.MaterialTextView;
 
 public class ParallelFragment extends Fragment {
 
-    private ParallelViewModel parallelViewModel;
+    private TextInputEditText textInputEditTextconst;
+    private MaterialTextView materialTextView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        parallelViewModel =
-                ViewModelProviders.of(this).get(ParallelViewModel.class);
+        ParallelViewModel parallelViewModel = ViewModelProviders.of(this).get(ParallelViewModel.class);
         View root = inflater.inflate(R.layout.fragment_parallel, container, false);
-        final TextView textView = root.findViewById(R.id.text_send);
+        final TextView textView = root.findViewById(R.id.title_parallel);
+        textInputEditTextconst = root.findViewById(R.id.edt_constants_parallel);
+        MaterialButton materialButton = root.findViewById(R.id.btn_calculate_parallel);
+        materialTextView = root.findViewById(R.id.tv_hasil_parallel);
+        materialButton.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                if (textInputEditTextconst.length() > 0) {
+                    Double constants = Double.valueOf(textInputEditTextconst.getText().toString());
+                    Double parallel = constants * 2;
+                    String hasil = String.valueOf(parallel);
+                    materialTextView.setText(hasil);
+                }
+                else {
+                    materialTextView.setText("Input Cannot be Empty");
+                }
+            }
+        });
         parallelViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
